53ed95c6336276222256ec8990faf224efa6cf7d
Gerry Sousa <gerry.nett@gmail.com>
2018-08-21 15:39:54 -0300

    Add referencias bases pages, necessario refatorar posteriormente

--- page/SplashPage.cs
+++ page/SplashPage.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using testes_appium_C_sharp.core;
 
 namespace testes_appium_C_sharp.page
 {
